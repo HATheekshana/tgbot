e7646a05877b9fdcfc6338be4680eca5ca1d2726
HATheekshana <h.a.supuntheekshana@gmail.com>
2026-03-19 06:27:59 +0530

    c

--- bot.py
+++ bot.py
@@ -13,17 +13,15 @@ from enka_api import fetch_enka_data ,get_user_uid
 from aiogram.filters import Command
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 import requests
-from aioenkanetworkcard import encbanner
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from datetime import datetime, timedelta
 from aiogram.types import FSInputFile, BufferedInputFile, InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import Command
 from pytz import timezone
-from card_gen import generate_profile_card
 from wishing import combine_images
-from genshin_utils import get_character_name ,get_exploration_data,get_abyss_data,get_player_full_data,parse_character_data
-from data import weapons3, characters4, characters5, rare ,CHAR_NAMES
+from genshin_utils import get_char_name ,get_exploration_data,get_abyss_data,get_player_full_data,parse_character_data
+from data import weapons3, characters4, characters5, rare
 
 ITEMS_PER_PAGE = 10
 dp = Dispatcher()
