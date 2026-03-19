993725cd42bf7a11771590a4a487f5c01ed4feb1
HATheekshana <h.a.supuntheekshana@gmail.com>
2026-03-19 07:56:28 +0530

    h

--- bot.py
+++ bot.py
@@ -21,7 +21,7 @@ from aiogram.filters import Command
 from pytz import timezone
 from wishing import combine_images
 from genshin_utils import get_character_name ,get_exploration_data,get_abyss_data,get_player_full_data,parse_character_data
-from data import weapons3, characters4, characters5, rare ,CHAR_NAMES
+from data import weapons3, characters4, characters5, rare 
 
 ITEMS_PER_PAGE = 10
 dp = Dispatcher()
