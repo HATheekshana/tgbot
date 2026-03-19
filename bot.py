c78e2be305d0cc5c110af6799a1c5edf9ac2b29f
HATheekshana <h.a.supuntheekshana@gmail.com>
2026-03-19 06:07:35 +0530

    characters

--- bot.py
+++ bot.py
@@ -12,7 +12,6 @@ from database import users_col, cluster
 from enka_api import fetch_enka_data
 from aiogram.filters import Command
 from aiogram.utils.keyboard import InlineKeyboardBuilder
-from enkapnetwork import EnkaNetworkAPI
 import requests
 from aioenkanetworkcard import encbanner
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
