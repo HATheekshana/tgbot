94c83c18284cd0d97dddd58a590fb8e7dc4bbf72
HATheekshana <h.a.supuntheekshana@gmail.com>
2026-03-21 16:02:28 +0530

    \n

--- bot.py
+++ bot.py
@@ -893,12 +893,12 @@ async def my_profile(message: types.Message):
     msg += f"𖹭 <b>Achievements:</b> {user_info['achievements']}\n"
     msg+=f"𖹭 <b>Days:</b> {user_info['days_active']}\n"
     if user_info_enka['signature']:
-        msg += f"<i>\"{user_info_enka['signature']}\"</i>\n\n"
+        msg += f"<i>\"{user_info_enka['signature']}\"</i>\n"
         
     msg += "─────── ⋆⋅𖤓⋅⋆ ───────\n\n"
 
     # Exploration Section
-    msg += "<b> EXPLORATION</b>\n\n"
+    msg += "<b> EXPLORATION</b>\n"
     msg += "⊹ ࣪ ﹏﹏𓊝﹏𓂁﹏﹏⊹ ࣪ ˖\n\n"
     for area in exploration_data:
         # :15 ensures the percentages stay aligned in a column
