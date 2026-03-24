fa386a6cde9253a6a4aaab1b65486fe7fe8b8a63
HATheekshana <h.a.supuntheekshana@gmail.com>
2026-03-24 20:00:16 +0530

    final

--- t_c.py
+++ t_c.py
@@ -115,7 +115,6 @@ def draw_build_column(canvas, start_x, data,t_icons, c_icons):
         is_locked = i >= data['cons_count']
         c_bgs_res = con_bg.resize((70, 70), Image.Resampling.LANCZOS)
         canvas.paste(c_bgs_res, (x, y), c_bgs_res)
-        draw.ellipse([x+10, y+10, x+60, y+60], fill=(0, 0, 0, 160))
         img = icon.resize((60, 60), Image.Resampling.LANCZOS)
         c_mask = mask.resize((60, 60), Image.Resampling.LANCZOS)
         canvas.paste(img, (x+5, y+5), c_mask)
