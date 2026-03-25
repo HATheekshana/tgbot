f5671327b6bdf53e6388c2a666cc4bb0b3b6b344
HATheekshana <h.a.supuntheekshana@gmail.com>
2026-03-25 12:23:44 +0530

    try

--- char_compare.py
+++ char_compare.py
@@ -351,8 +351,8 @@ async def compare_characters(uid, uid2, char_id):
                                radius=8, fill=(15, 15, 25, 170), outline=(255,255,255,50))
         val2 = fmt.format(stats_them.get(key, 0)) if stats_them else "0"
         draw.text((v2_x + (val_w // 2), curr_y + (row_height//2)), val2, font=font, fill=(255, 255, 255), anchor="mm")
-    draw_build_column(background, 785, them_data, t_icons, c_icons)
-    draw_build_column(background, 935, me_data, t_icons, c_icons) 
+    draw_build_column(ui_layer, 785, them_data, t_icons, c_icons)
+    draw_build_column(ui_layer, 935, me_data, t_icons, c_icons) 
 
     if rarity == 4:
         star4 = Image.open("asstests/icons/stars/c_stars_4.png").convert("RGBA")
