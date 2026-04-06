593560ff77a5359887c3a16a18ada94ccd75eb42
HATheekshana <h.a.supuntheekshana@gmail.com>
2026-04-06 08:02:18 +0530

    test

--- api.py
+++ api.py
@@ -2,8 +2,7 @@ import os  # ADD THIS
 from dotenv import load_dotenv
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
-import motor
-AsyncIOMotorClient = motor.motor_async_engine.AsyncIOMotorClient
+import motor.motor_async_engine
 import httpx
 import logging
 load_dotenv()
