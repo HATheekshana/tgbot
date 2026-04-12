d46028cacd48a59eb243384648b4d10e94ab417a
HATheekshana <h.a.supuntheekshana@gmail.com>
2026-04-12 10:15:38 +0530

    test

--- character_card.py
+++ character_card.py
@@ -302,7 +302,7 @@ async def characters_card(uid, char_id, telegram_id):
                 if complete_graph is not None:
                     # Draw background
                     radar_bg = Image.open("asstests/icons/radar_bg.png").convert("RGBA")
-                    radar_bg = radar_bg.resize((530, 525), Image.Resampling.BILINEAR)
+                    radar_bg = radar_bg.resize((510, 505), Image.Resampling.BILINEAR)
                     ui_layer.paste(radar_bg, (graph_position[0] - 75, graph_position[1] - 60), radar_bg)
                     
                     # Draw graph
